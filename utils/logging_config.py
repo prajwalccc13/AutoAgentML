e272b877e9b5c55df21ebed08631a5177867050d
Prajwal Chaudhary <prajwalccc13@gmail.com>
2026-08-02 02:07:29 +1000

    added logging

---
+++ utils/logging_config.py
@@ -0,0 +1,9 @@
+import logging
+
+
+def setup_logging(level=logging.INFO):
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s %(levelname)s [%(name)s] %(message)s",
+        datefmt="%H:%M:%S",
+    )
